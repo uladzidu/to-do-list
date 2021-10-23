8429b80d85781e0f5d49b30605c3ee55c4d0592a
Uladzislau Dukhounik <vdukhovnik1@gmail.com>
2021-10-23 20:38:59 +0200

    added removeTask function (simple to understand)

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { ArrayType, Todolist }  from './Todolist';
 
 function App() {
 
-let tasks1 : Array<ArrayType> = [
+let tasks : Array<ArrayType> = [
     {id : 1 , title : "HTML", isDone : true },
     {id : 2 , title : "JS", isDone : true },
     {id : 3 , title : "React", isDone : false },
@@ -12,12 +12,25 @@ let tasks1 : Array<ArrayType> = [
     {id : 5 , title : "Python", isDone : false },
 ]
 
-
+function removeTask (id : number) {
+    debugger
+    let resultTasks = tasks.filter( (t) => {
+        if (t.id !== id ) {
+            return true;
+        } else {
+            return false;
+        }
+    }  )
+}
 
 
     return (
         <div className="App">
-            <Todolist title = "What to learn" tasks = {tasks1} /> 
+            <Todolist 
+            title = "What to learn" 
+            tasks = {tasks} 
+            removeTask = {removeTask} 
+            />
         </div>
     );
 }
