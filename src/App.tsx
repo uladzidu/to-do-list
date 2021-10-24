504003772d0d6da991815126a7367c86b519b2eb
Uladzislau Dukhounik <vdukhovnik1@gmail.com>
2021-10-24 23:46:17 +0200

    24.10

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
+//import { useState } from '@testing-library/react/node_modules/@types/react';
 import React from 'react';
 import './App.css';
-import { ArrayType, Todolist }  from './Todolist';
+import { ArrayType, Todolist } from './Todolist';
+import { useState } from 'react';
 
 function App() {
 
-let tasks : Array<ArrayType> = [
-    {id : 1 , title : "HTML", isDone : true },
-    {id : 2 , title : "JS", isDone : true },
-    {id : 3 , title : "React", isDone : false },
-    {id : 4 , title : "Redux", isDone : false },
-    {id : 5 , title : "Python", isDone : false },
-]
+    let initTasks: Array<ArrayType> = [
+        { id: 1, title: "HTML", isDone: true },
+        { id: 2, title: "JS", isDone: true },
+        { id: 3, title: "React", isDone: false },
+        { id: 4, title: "Redux", isDone: false },
+        { id: 5, title: "Python", isDone: false },
+    ]
 
-function removeTask (id : number) {
-    debugger
-    let resultTasks = tasks.filter( (t) => {
-        if (t.id !== id ) {
-            return true;
-        } else {
-            return false;
-        }
-    }  )
-}
+    let arr = useState(initTasks)
+
+    let tasks = arr[0];
+    let setTasks = arr[1];
+
+
+    function removeTask(id: number) {
+        let filteredTasks = tasks.filter(t => t.id !== id)
+        setTasks(filteredTasks)
+    }
 
 
     return (
         <div className="App">
-            <Todolist 
-            title = "What to learn" 
-            tasks = {tasks} 
-            removeTask = {removeTask} 
+            <Todolist
+                title="What to learn"
+                tasks={tasks}
+                removeTask={removeTask}
             />
         </div>
     );
