632b20227220aca569ea4cb9c47b5248306f634e
master <vdukhovnik1@gmail.com>
2022-01-17 21:46:19 +0100

    17.01

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import './App.css';
-import {Todolist} from "./Todolist";
+import {taskPropsType, Todolist} from "./Todolist";
 
 
-export function App () {
+export function App() {
+
+    let tasks1: Array<taskPropsType>  = [
+        {id: 1, title: "Css", isDone: true},
+        {id: 2, title: "Js", isDone: false},
+        {id: 3, title: "React", isDone: false},
+    ]
+
+    let tasks2: Array<taskPropsType> = [
+        {id: 1, title: "Movie 1", isDone: true},
+        {id: 2, title: "Movie 2", isDone: false},
+        {id: 3, title: "Movie 3", isDone: true},
+    ]
+
+
     return (
         <div>
-            <Todolist/>
+            <Todolist title={"What to learn"} tasks={tasks1}/>
+            <Todolist title={"Movies"} tasks={tasks2}/>
+
         </div>
     )
 }
